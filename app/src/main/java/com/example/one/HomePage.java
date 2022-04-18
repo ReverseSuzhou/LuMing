021caf1dd0788dcbb92ce9b03e03fb4e9ba8b575
notture <2622759107@qq.com>
2022-04-18 12:17:40 +0800

    tab

--- app/src/main/java/com/example/one/HomePage.java
+++ app/src/main/java/com/example/one/HomePage.java
@@ -1,9 +1,12 @@
 package com.example.one;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -20,11 +23,20 @@ import android.widget.Toast;
 
 import com.example.one.Adapter.HomeAdapter;
 import com.example.one.Bean.Push;
+import com.example.one.Fragment.Fragment_HomeFour;
+import com.example.one.Fragment.Fragment_HomeThree;
+import com.example.one.Fragment.Fragment_HomeTwo;
+import com.example.one.Fragment.Fragment_Homeone;
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItem;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
 
 import org.xml.sax.SAXException;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
@@ -37,21 +49,14 @@ public class HomePage extends AppCompatActivity {
     private RadioButton mBtn_message;
     private RadioButton mBtn_personal;
     private TextView tv_search;
-    private SwipeRefreshLayout swipe_home;
-    private RecyclerView rv_home;
-    private TextView error_home;
-    List<Push> data = new LinkedList<>();
-    private HomeAdapter adapter;
-    DBUtils db;
-    ResultSet rs;
-    Thread t;
 
     private Button hotspot;
     private Button reco;
     private Button study;
     private Button sport;
 
-
+    private SmartTabLayout smartTabLayout;
+    private ViewPager viewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -63,11 +68,10 @@ public class HomePage extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = null;
-                intent = new Intent(HomePage.this,SearchActivity.class);
+                intent = new Intent(HomePage.this, SearchActivity.class);
                 startActivity(intent);
             }
         });
-
 
 
 //↓↓↓↓↓↓↓↓底下五个按钮的跳转功能↓↓↓↓↓↓↓↓
@@ -79,30 +83,17 @@ public class HomePage extends AppCompatActivity {
         mBtn_editor = findViewById(R.id.rb_add);
         mBtn_message = findViewById(R.id.rb_message);
         mBtn_personal = findViewById(R.id.rb_user);
-        swipe_home = findViewById(R.id.swipe_home);
-        rv_home = findViewById(R.id.rv_home);
-        error_home = findViewById(R.id.error_home);
+
         hotspot = findViewById(R.id.main_page_2_button_HotSpot);
         reco = findViewById(R.id.main_page_2_button_Recommend);
         study = findViewById(R.id.main_page_2_button_Study);
         sport = findViewById(R.id.main_page_2_button_Sport);
 
+        initView();
+        viewPager.setOffscreenPageLimit(5);
 
+        initTab();
 
-
-
-
-        swipe_home.setColorSchemeResources(android.R.color.holo_green_light,android.R.color.holo_red_light,android.R.color.holo_blue_light);
-        swipe_home.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                try {
-                    Refresh();
-                } catch (SQLException throwables) {
-                    throwables.printStackTrace();
-                }
-            }
-        });
         //主页面
         mBtn_home.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -117,7 +108,7 @@ public class HomePage extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = null;
-                intent = new Intent(HomePage.this,AssociationActivity.class);
+                intent = new Intent(HomePage.this, AssociationActivity.class);
                 startActivity(intent);
             }
         });
@@ -126,7 +117,7 @@ public class HomePage extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = null;
-                intent = new Intent(HomePage.this,EditorActivity.class);
+                intent = new Intent(HomePage.this, EditorActivity.class);
                 startActivity(intent);
             }
         });
@@ -144,283 +135,42 @@ public class HomePage extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = null;
-                intent = new Intent(HomePage.this,PersonalActivity.class);
+                intent = new Intent(HomePage.this, PersonalActivity.class);
                 intent.putExtra("username", new SaveSharedPreference().getUsername());
                 startActivity(intent);
             }
         });
-        hotspot.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                swipe_home.setRefreshing(false);
-                data.clear();
-                try {
-                    t = new Thread(new Runnable() {
-                        @Override
-                        public void run() {
-                            db = new DBUtils();
-                            rs = db.query("select * from forumt order by F_likenum desc;");
-                            try {
-                                while(rs.next()){
-                                    Push po = new Push();
-                                    po.setForumt_id(rs.getString("Forumt_id"));
-                                    po.setForumt_date(rs.getString("Forumt_date"));
-                                    po.setForumt_title(rs.getString("F_title"));
-                                    po.setForumt_content(rs.getString("Forumt_content"));
-                                    po.setF_likenum(rs.getInt("F_likenum"));
-                                    po.setF_collectnum(rs.getInt("F_collectnum"));
-                                    po.setF_commentnum(rs.getInt("F_commentnum"));
-                                    po.setUsername(rs.getString("User_name"));
-                                    po.setUser_phone(rs.getString("User_phone"));
-                                    data.add(po);
-                                };
-                            } catch (SQLException throwables) {
-                                throwables.printStackTrace();
-                            }
-                            try {
-                                db.connection.close();
-                            } catch (SQLException throwables) {
-                                throwables.printStackTrace();
-                            }
-
-                        }
-                    });
-                    t.start();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                while(t.isAlive() == true);
-                if(data.size()>0){
-                    swipe_home.setRefreshing(false);
-                    swipe_home.setVisibility(View.VISIBLE);
-                    adapter = new HomeAdapter(HomePage.this,data);
-                    rv_home.setLayoutManager(new LinearLayoutManager(HomePage.this));
-                    rv_home.setAdapter(adapter);
-                }
-                else {
-                    error_home.setVisibility(View.VISIBLE);
-                }
-            }
-        });
-        reco.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                swipe_home.setRefreshing(false);
-                data.clear();
-                try {
-                    t = new Thread(new Runnable() {
-                        @Override
-                        public void run() {
-                            db = new DBUtils();
-                            rs = db.query("select * from forumt order by Forumt_date desc;");
-                            try {
-                                while(rs.next()){
-                                    Push po = new Push();
-                                    po.setForumt_id(rs.getString("Forumt_id"));
-                                    po.setForumt_date(rs.getString("Forumt_date"));
-                                    po.setForumt_title(rs.getString("F_title"));
-                                    po.setForumt_content(rs.getString("Forumt_content"));
-                                    po.setF_likenum(rs.getInt("F_likenum"));
-                                    po.setF_collectnum(rs.getInt("F_collectnum"));
-                                    po.setF_commentnum(rs.getInt("F_commentnum"));
-                                    po.setUsername(rs.getString("User_name"));
-                                    po.setUser_phone(rs.getString("User_phone"));
-                                    data.add(po);
-                                };
-                            } catch (SQLException throwables) {
-                                throwables.printStackTrace();
-                            }
-                            try {
-                                db.connection.close();
-                            } catch (SQLException throwables) {
-                                throwables.printStackTrace();
-                            }
-
-                        }
-                    });
-                    t.start();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                while(t.isAlive() == true);
-                if(data.size()>0){
-                    swipe_home.setRefreshing(false);
-                    swipe_home.setVisibility(View.VISIBLE);
-                    adapter = new HomeAdapter(HomePage.this,data);
-                    rv_home.setLayoutManager(new LinearLayoutManager(HomePage.this));
-                    rv_home.setAdapter(adapter);
-                }
-                else {
-                    error_home.setVisibility(View.VISIBLE);
-                }
-            }
-        });
-        study.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                swipe_home.setRefreshing(false);
-                data.clear();
-                try {
-                    t = new Thread(new Runnable() {
-                        @Override
-                        public void run() {
-                            db = new DBUtils();
-                            rs = db.query("select * from forumt where F_lable = 'study' order by Forumt_date desc;");
-                            try {
-                                while(rs.next()){
-                                    Push po = new Push();
-                                    po.setForumt_id(rs.getString("Forumt_id"));
-                                    po.setForumt_date(rs.getString("Forumt_date"));
-                                    po.setForumt_title(rs.getString("F_title"));
-                                    po.setForumt_content(rs.getString("Forumt_content"));
-                                    po.setF_likenum(rs.getInt("F_likenum"));
-                                    po.setF_collectnum(rs.getInt("F_collectnum"));
-                                    po.setF_commentnum(rs.getInt("F_commentnum"));
-                                    po.setUsername(rs.getString("User_name"));
-                                    po.setUser_phone(rs.getString("User_phone"));
-                                    data.add(po);
-                                };
-                            } catch (SQLException throwables) {
-                                throwables.printStackTrace();
-                            }
-                            try {
-                                db.connection.close();
-                            } catch (SQLException throwables) {
-                                throwables.printStackTrace();
-                            }
-
-                        }
-                    });
-                    t.start();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                while(t.isAlive() == true);
-                if(data.size()>0){
-                    swipe_home.setRefreshing(false);
-                    swipe_home.setVisibility(View.VISIBLE);
-                    adapter = new HomeAdapter(HomePage.this,data);
-                    rv_home.setLayoutManager(new LinearLayoutManager(HomePage.this));
-                    rv_home.setAdapter(adapter);
-                }
-                else {
-                    error_home.setVisibility(View.VISIBLE);
-                }
-            }
-        });
-        sport.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                swipe_home.setRefreshing(false);
-                data.clear();
-                try {
-                    t = new Thread(new Runnable() {
-                        @Override
-                        public void run() {
-                            db = new DBUtils();
-                            rs = db.query("select * from forumt where F_lable = 'sport' order by Forumt_date desc;");
-                            try {
-                                while(rs.next()){
-                                    Push po = new Push();
-                                    po.setForumt_id(rs.getString("Forumt_id"));
-                                    po.setForumt_date(rs.getString("Forumt_date"));
-                                    po.setForumt_title(rs.getString("F_title"));
-                                    po.setForumt_content(rs.getString("Forumt_content"));
-                                    po.setF_likenum(rs.getInt("F_likenum"));
-                                    po.setF_collectnum(rs.getInt("F_collectnum"));
-                                    po.setF_commentnum(rs.getInt("F_commentnum"));
-                                    po.setUsername(rs.getString("User_name"));
-                                    po.setUser_phone(rs.getString("User_phone"));
-                                    data.add(po);
-                                };
-                            } catch (SQLException throwables) {
-                                throwables.printStackTrace();
-                            }
-                            try {
-                                db.connection.close();
-                            } catch (SQLException throwables) {
-                                throwables.printStackTrace();
-                            }
-
-                        }
-                    });
-                    t.start();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                while(t.isAlive() == true);
-                if(data.size()>0){
-                    swipe_home.setRefreshing(false);
-                    swipe_home.setVisibility(View.VISIBLE);
-                    adapter = new HomeAdapter(HomePage.this,data);
-                    rv_home.setLayoutManager(new LinearLayoutManager(HomePage.this));
-                    rv_home.setAdapter(adapter);
-                }
-                else {
-                    error_home.setVisibility(View.VISIBLE);
-                }
-            }
-        });
-        try {
-            Refresh();
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-
-//↑↑↑↑↑↑↑↑底下五个按钮的跳转功能↑↑↑↑↑↑↑↑
-
 
     }
+    private void initView() {
+        smartTabLayout = findViewById(R.id.hometab);
+        viewPager = findViewById(R.id.homevp);
+    }
 
-    private void Refresh() throws SQLException {
-        swipe_home.setRefreshing(false);
-        data.clear();
-        try {
-            t = new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    db = new DBUtils();
-                    rs = db.query("select * from forumt order by Forumt_date desc;");
-                    try {
-                        while(rs.next()){
-                            Push po = new Push();
-                            po.setForumt_id(rs.getString("Forumt_id"));
-                            po.setForumt_date(rs.getString("Forumt_date"));
-                            po.setForumt_title(rs.getString("F_title"));
-                            po.setForumt_content(rs.getString("Forumt_content"));
-                            po.setF_likenum(rs.getInt("F_likenum"));
-                            po.setF_collectnum(rs.getInt("F_collectnum"));
-                            po.setF_commentnum(rs.getInt("F_commentnum"));
-                            po.setUsername(rs.getString("User_name"));
-                            po.setUser_phone(rs.getString("User_phone"));
-                            data.add(po);
-                        };
-                    } catch (SQLException throwables) {
-                        throwables.printStackTrace();
-                    }
-                    try {
-                        db.connection.close();
-                    } catch (SQLException throwables) {
-                        throwables.printStackTrace();
-                    }
 
-                }
-            });
-            t.start();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        while(t.isAlive() == true);
-        if(data.size()>0){
-            swipe_home.setRefreshing(false);
-            swipe_home.setVisibility(View.VISIBLE);
-            adapter = new HomeAdapter(HomePage.this,data);
-            rv_home.setLayoutManager(new LinearLayoutManager(HomePage.this));
-            rv_home.setAdapter(adapter);
-        }
-        else {
-            error_home.setVisibility(View.VISIBLE);
-        }
+    private void initTab() {
+        FragmentPagerItemAdapter ad = new FragmentPagerItemAdapter(getSupportFragmentManager(),
+                FragmentPagerItems.with(this)
+                        .add("热点", Fragment_Homeone.class).add("推荐",Fragment_HomeTwo.class)
+                        .add("学习", Fragment_HomeThree.class).add("运动", Fragment_HomeFour.class)
+                        .create());
+        viewPager.setAdapter(ad);
+        smartTabLayout.setViewPager(viewPager);
+        smartTabLayout.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
 
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                viewPager.setCurrentItem(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
     }
 }
