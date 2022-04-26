7f0e0266c303a7f2471759b2c613a2e9234d2ba1
SandFish <1579264622@qq.com>
2022-04-27 00:13:52 +0800

    优化

--- app/src/main/java/com/example/one/MessageActivity.java
+++ app/src/main/java/com/example/one/MessageActivity.java
@@ -68,8 +68,8 @@ public class MessageActivity extends AppCompatActivity {
 
         SaveSharedPreference user = new SaveSharedPreference();
         sql[0] = "select * from notice order by Notice_time desc;";
-        sql[1] = "select * from user_forumt left join forumt on user_forumt.Forumt_id = forumt.Forumt_id where user_forumt.User_phone = '" + user.getPhone() + "';";
-        sql[2] = "select * from focus left join forumt on follow_phone = User_phone where focus_phone = '" + user.getPhone() + "' order by Forumt_date desc limit 10;";
+        sql[1] = "select * from user_forumt left join admin_forumt on user_forumt.Forumt_id = admin_forumt.Forumt_id where user_forumt.User_phone = '" + user.getPhone() + "';";
+        sql[2] = "select * from focus left join admin_forumt on follow_phone = User_phone where focus_phone = '" + user.getPhone() + "' order by Forumt_date desc limit 10;";
         sql[3] = "select * from user_forumt left join comment on user_forumt.Forumt_id = comment.Forumt_id where user_forumt.User_phone = '" + user.getPhone() + "' order by Comment_time desc;";
 
         swipe_message = findViewById(R.id.swipe_message);
